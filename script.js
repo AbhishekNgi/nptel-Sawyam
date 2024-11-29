6ebe9a5c3377b5e0b4cf2588411c2a4d614d1a8c
Abhishek Negi <160260424+AbhishekNgi@users.noreply.github.com>
2024-11-29 14:53:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('download-btn').addEventListener('click', () => {
+    // Redirect the user to the PDF file for download
+    window.location.href = './Untitled design (3).pdf';
+  });
+  
